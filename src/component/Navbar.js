4e85cd7986bcf8c3afe1442de133a65465bb09a2
Siddharth sharma <123795696+Shammy-sharma@users.noreply.github.com>
2023-07-04 17:08:52 +0530

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,37 +4,37 @@ export const Navbar = () => {
 
   return (
     <div className='container-fulid overflow-hidden '>
-      <nav class="navbar navbar-expand-lg navbar-light bg-white">
-        <div class="container-fluid">
-          <strong class="navbar-brand fs-4  " href="#">Logistica</strong>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-white">
+        <div className="container-fluid">
+          <strong className="navbar-brand fs-4  " href="#">Logistica</strong>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
-              <li class="nav-item">
-                <a class="nav-link active icon-link-hover" aria-current="page" href="#">  HOME</a>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
+              <li className="nav-item">
+                <a className="nav-link active icon-link-hover" aria-current="page" href="#">  HOME</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link  icon-link-hover" aria-current="page" href="#">
+              <li className="nav-item">
+                <a className="nav-link  icon-link-hover" aria-current="page" href="#">
                   CONTACT</a>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
              <strong className='number'> <HeadphonesIcon style={{color:"#ff3e41"}}/> +012 345 6789</strong> 
               </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle icon-link-hover" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle icon-link-hover" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   PAGES
                 </a>
                
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider" /></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
                  
                 </ul>
               </li>
